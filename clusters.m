e0f172e4332939b3a4893137488dd018d5a76257
Olivia Guest <guest.olivia@gmail.com>
2014-04-11 15:34:17 +0100

    clusters

--- clusters.m
+++ clusters.m
@@ -1,8 +1,10 @@
 clear all;
 % rogers_patterns;    
 
-labels;
-H = zeros(48-1, 20);
+% labels;
+% H = zeros(48-1, 20);
+load('tyler_patterns.mat')
+H = zeros(16-1, 20);
 % for l = 1:20,
     l = 1;
 
@@ -11,7 +13,7 @@ H = zeros(48-1, 20);
     %     clear T;
     %     figure;
 
-    eval(['tyler_' num2str(l-1) ';']);
+    eval(['tyler_reccurent_' num2str(l-1) ';']);
 
 %     eval(['bptt_category_patterns_2_all_clamped_1_' num2str(l-1) ';']);
 
@@ -21,14 +23,17 @@ H = zeros(48-1, 20);
         f = figure('units','normalized','position',[.1 .1 700 800])
 
 
-    H(:, l) = dendrogram(Z, 0, 'colorthreshold', 0 , 'labels', S, 'orientation', 'right');
+%     H(:, l) = dendrogram(Z, 0, 'colorthreshold', 0 , 'labels', S, 'orientation', 'right');
+    H(:, l) = dendrogram(Z, 0, 'colorthreshold', 0 , 'labels', labels, 'orientation', 'right');
     set(H(:, l), 'LineWidth', 2);
     xlabel('Euclidean distance');
-    eval(['title ''Internal Representations Given Name Input at ' num2str((l-1)*100.0/20.0) '%''']);
+%     eval(['title ''Internal Representations Given Name Input at ' num2str((l-1)*100.0/20.0) '%''']);
+   title 'Internal Representations'
 % title 'Visual Input Patterns'
-    set(0,'defaultaxesfontname', 'Times');
-    set(0,'defaulttextfontname', 'Times');
-    saveas(f, eval(['''dendrogram_rogers_pattern_wise_' num2str(l-1) '.pdf''']), 'pdf');
+%     set(0,'defaultaxesfontname', 'Times');
+%     set(0,'defaulttextfontname', 'Times');
+%     saveas(f, eval(['''dendrogram_rogers_pattern_wise_' num2str(l-1) '.pdf''']), 'pdf');
+    saveas(f, eval(['''tyler_recurrent_' num2str(l-1) '.pdf''']), 'pdf');
 
 %cophenet(Z,pdist(X))
 % end
